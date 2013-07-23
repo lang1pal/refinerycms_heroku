bac20226a2f340e1ed6daf7ef6a0dee13de569e9
lang1pal <lang1pal@gmail.com>
2013-07-23 09:03:59 -0400

    Created application using Refinery CMS 2.0.10

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefinerycmsHeroku::Application.config.secret_token = '5ba06744403ff0244a23418411cb91107e5641c098a7665ebd949af255679085286f6bf878a9d27a0c8e58339cb524a7a32d2276f780686fc6566a98c958d8a4'
